bdeea80f00c49e02db8fe0afd6d821a3e630da3a
Mingxi Zhou <mzhou@uri.edu>
2024-05-05 00:08:04 -0400

    finished edit waypoints

--- mvp_gui/routes.py
+++ mvp_gui/routes.py
@@ -54,6 +54,7 @@ def update_load():
             turbo.push(turbo.replace(render_template("tables/health_table.html"), 'power_health'))
             turbo.push(turbo.replace(render_template("tables/pose_table.html"), 'pose_info'))
             turbo.push(turbo.replace(render_template("tables/power_manager_table.html"), 'power_manager'))
+            # turbo.push(turbo.replace(render_template("tables/waypoints_table.html"), 'mission_waypoints'))
             # turbo.push(turbo.replace(render_template("tables/map_table.html"), 'map'))
 
 @app.context_processor
@@ -93,19 +94,67 @@ def power_manager_page():
             # return render_template("power_manager.html", items=items, vitals=vitals)
     return render_template("power_manager.html", items=items, vitals=vitals)
 
+
 @app.route("/map", methods=['GET', 'POST'])
 def map_page():
     print("map lnk")
     return render_template("map.html")
 
+
 @app.route("/mission", methods=['GET', 'POST'])
 def mission_page():
-    waypoints = Waypoints.query.all()
+    waypoints = Waypoints.query.order_by(Waypoints.id).all()
     if request.method == 'POST':
         action = request.form.get('action')
         if action == str("add"):
-            return redirect(url_for('add_waypoint_page'))
+            return redirect(url_for('add_waypoint_page'))   
+
+        ##delete a waypoint
+        if 'delete' in request.form:
+            delete_id = request.form['delete']
+            entry = Waypoints.query.get(delete_id)
+            if entry:
+                db.session.delete(entry)
+                db.session.commit()
+                return redirect(url_for('mission_page'))
+    
+        ##edit a waypoint
+        elif 'edit' in request.form:
+            edit_id = request.form['edit']
+            entry = Waypoints.query.get(edit_id)
+            if entry:
+                return redirect(url_for('edit_waypoint_page', edit_id=edit_id))   
     return render_template("mission.html", items=waypoints)
+
+
+
+@app.route('/mission/edit_waypoint', methods=['GET','POST'])
+def edit_waypoint_page():
+    edit_id = request.args.get('edit_id')
+    entry = Waypoints.query.get(edit_id)
+    form = WaypointForm()
+   
+    if form.validate_on_submit():
+        db.session.delete(entry)
+        db.session.commit()
+        waypoint = Waypoints(id=form.id.data,
+                           type=form.type.data,
+                           lat=form.lat.data,
+                           lon=form.lon.data,
+                           x=form.x.data,
+                           y=form.y.data,
+                           z=form.z.data,
+                           )
+        # entry = waypoint
+
+        # entry.type = form.type.data
+        db.session.add(waypoint)
+        # print("data=", entry.type)
+        db.session.commit()
+
+        return redirect(url_for('mission_page'))
+    return render_template('edit_waypoints.html', form=form, entry=entry)
+
 
 @app.route('/mission/waypoints', methods=['GET','POST'])
 def add_waypoint_page():
@@ -124,8 +173,7 @@ def add_waypoint_page():
         db.session.add(waypoint)
         db.session.commit()
         return redirect(url_for('mission_page'))
-    return render_template('add_waypoints.html', form=form
-    )
+    return render_template('add_waypoints.html', form=form)
 
 
 @app.route("/monitor")
